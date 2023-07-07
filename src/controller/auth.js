03374c46185c3b4d0a968842379cc055fdaa0833
AlulCode45 <alulgans70@gmail.com>
2023-07-07 08:55:27 +0700

    [ADD] Feature refresh token and get user

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,6 +1,12 @@
 const {dbConnect} = require("../config/connection_db");
 const bcrypt = require('bcrypt')
 
+const getBearerToken = (token) => {
+    if(typeof token !== 'undefined'){
+        return token.split(' ')[1]
+    }
+}
+
 const authLogin = async (req,res) => {
     const data = await dbConnect.table('users')
         .where('username','=',req?.body?.username)
@@ -12,7 +18,7 @@ const authLogin = async (req,res) => {
     if(data){
         if(bcrypt.compareSync(req?.body?.password,data?.password)){
             const randomToken = require('random-token').create('abcdefghijklmnopqrstuvwxzyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|\/~.,');
-            const token = randomToken(42);
+            const token = randomToken(62);
             const changeToken = await dbConnect.table('users')
                 .where('username','=',req?.body?.username)
                 .first('*')
@@ -20,6 +26,7 @@ const authLogin = async (req,res) => {
                     token: token
                 })
                 .then((out) => {
+                    data['token'] = token
                     return true
                 }).catch((err) => {
                     return false
@@ -46,7 +53,7 @@ const authRegister = async (req,res) => {
     const hash = bcrypt.hashSync(req?.body?.password, salt);
 
     const randomToken = require('random-token').create('abcdefghijklmnopqrstuvwxzyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|\/~.,');
-    const token = randomToken(42);
+    const token = randomToken(62);
 
     const data = {
         name:  req?.body?.name,
@@ -64,10 +71,58 @@ const authRegister = async (req,res) => {
                 massage:"Register successfully!"
             })
         })
+}
 
+const authRefreshToken = async (req,res)=> {
+    const token = getBearerToken(req?.headers?.authorization)
+
+    const randomToken = require('random-token').create('abcdefghijklmnopqrstuvwxzyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|\/~.,');
+    const newToken = randomToken(62);
+
+    const data = await dbConnect.table('users')
+        .where('token','=',token)
+        .first('*')
+        .then((out) => {
+            return out
+        })
+
+    return await dbConnect.table('users')
+        .where('id',data?.id)
+        .first('token')
+        .update({
+            token: newToken
+        }).then((out) => {
+            res.json({
+                status:true,
+                massage: "Token refresh",
+                token: newToken
+            })
+        }).catch((err) => {
+            res.json({
+                status:false,
+                massage: "Token not refresh"
+            })
+        })
+}
+
+const authMe = async (req,res) => {
+    const token = getBearerToken(req?.headers?.authorization)
+    return await dbConnect.table('users')
+        .where('token','=',token)
+        .first('*')
+        .then((out) => {
+            res.json({
+                status:true,
+                massage: "Get successfully",
+                data: out
+            })
+        })
 }
 
 module.exports = {
     authLogin,
-    authRegister
+    authRegister,
+    authRefreshToken,
+    getBearerToken,
+    authMe
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const {authLogin, authRegister} = require("../controller/auth");
+const {authLogin, authRegister, authRefreshToken, authMe} = require("../controller/auth");
 const route = express.Router()
 
 route.post('/login',authLogin)
 route.post('/register',authRegister)
+route.post('/refresh',authRefreshToken)
+route.get('/me',authMe)
 
 module.exports = route
